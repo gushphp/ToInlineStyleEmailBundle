27aa476ff8d6f3cb0bdeece8a602a143bad86ab9
robertotru <roberto.trunfio@gmail.com>
2013-01-16 16:51:37 +0100

    Namespace updated

--- Converter/MissingTemplatingEngineException.php
+++ Converter/MissingTemplatingEngineException.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace RobertoTruToInlineStyleEmailBundle\Converter;
+namespace RobertoTru\ToInlineStyleEmailBundle\Converter;
 
 /**
  * Exception for missing TwigEngine.
