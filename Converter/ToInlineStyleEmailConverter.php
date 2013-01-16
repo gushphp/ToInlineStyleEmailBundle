acabe2fbeadeba1624ed63d4aa425fdd3d43a6d4
robertotru <roberto.trunfio@gmail.com>
2013-01-16 15:01:40 +0100

    Updated Converter Class

--- Converter/ToInlineStyleEmailConverter.php
+++ Converter/ToInlineStyleEmailConverter.php
@@ -3,9 +3,9 @@
  * This file is part of ToInlineStyleEmailBundle.
  *
  * (c) Roberto Trunfio <roberto@trunfio@it>
- * 
- * Part of the functions and fields description is taken from CSSToInlineStyle 
- * project by Tijs Verkoyen <php-css-to-inline-styles@verkoyen.eu> 
+ *
+ * Part of the functions and fields description is taken from CSSToInlineStyle
+ * project by Tijs Verkoyen <php-css-to-inline-styles@verkoyen.eu>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,43 +17,42 @@ use Symfony\Component\Templating\EngineInterface;
 use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
 
 /**
- * ToInlineStyleEmailConverter is a shortcut for using in Symfony2 the utility 
+ * ToInlineStyleEmailConverter is a shortcut for using in Symfony2 the utility
  * CssToInlineStyles developed by Tijs Verkoyen.
- *  
+ *
  * The suggested use of this utility is as a service.
  *
  * @author Roberto Trunfio <roberto@trunfio@it>
  */
 class ToInlineStyleEmailConverter {
-    
+
     /**
      * The EngineInterface used to render the templates. This is optional.
-     * 
+     *
      * @var Symfony\Component\Templating\EngineInterface
      */
     private $templating_engine;
-    
+
     /**
      * The class used for CSS-to-inline-style conversion.
-     * 
+     *
      * @var TijsVerkoyen\CssToInlineStyles\CssToInlineStyles
      */
     protected $cssToInlineStyles;
     /**
      * A string that represents the HTML file to be sent. Twig templates must be
      * rendered and then passed to this class.
-     * 
-     * @var string 
+     *
+     * @var string
      */
     protected $html;
     /**
      * A string that contains the CSS for the HTML file.
-     * 
-     * @var string 
+     *
+     * @var string
      */
     protected $css;
-    
-    
+
     /**
      * Should the generated HTML be cleaned
      *
@@ -67,7 +66,7 @@ class ToInlineStyleEmailConverter {
      * @var	string
      */
     protected $encoding = 'UTF-8';
-    
+
     /**
      * Use inline-styles block as CSS
      *
@@ -81,28 +80,25 @@ class ToInlineStyleEmailConverter {
      * @var bool
      */
     protected $stripOriginalStyleTags = false;
-    
 
     /**
      * Construct the service.
-     * 
-     * @param Symfony\Component\Templating\EngineInterface[optional] $templating_engine is the engine 
-     * for twig templates. This is optional. Set this param when configuring this 
+     *
+     * @param Symfony\Component\Templating\EngineInterface[optional] $templating_engine is the engine
+     * for twig templates. This is optional. Set this param when configuring this
      * class as a service.
      */
     public function __construct(EngineInterface $templating = null) {
         $this->templating_engine = $templating_engine;
-        $this->cssToInlineStyles = new CssToInlineStyles();             
+        $this->cssToInlineStyles = new CssToInlineStyles();
     }
-    
-    
-      
+
     /**
-     * If set to true, this function will activate the removal of the 
+     * If set to true, this function will activate the removal of the
      * IDs and classes inside the HTML document during conversion.
-     * 
+     *
      * This option is false by default.
-     * 
+     *
      *
      * @param  bool[optional] $on Should we enable cleanup?
      */
@@ -110,11 +106,11 @@ class ToInlineStyleEmailConverter {
         $this->cleanup = (bool) $on;
         $this->cssToInlineStyles->setCleanup($this->cleanup);
     }
-    
+
     /**
      * Set use of inline styles block
      * If this is enabled the class will use the style-block in the HTML.
-     * 
+     *
      * This option is false by default.
      *
      * @param  bool[optional] $on Should we process inline styles?
@@ -123,11 +119,11 @@ class ToInlineStyleEmailConverter {
         $this->useInlineStylesBlock = (bool) $on;
         $this->cssToInlineStyles->setUseInlineStylesBlock($this->useInlineStylesBlock);
     }
-    
+
     /**
      * Set strip original style tags.
      * If this is enabled the class will remove all style tags in the HTML.
-     * 
+     *
      * This option is false by default.
      *
      * @param  bool[optional] $onShould we process inline styles?
@@ -136,7 +132,7 @@ class ToInlineStyleEmailConverter {
         $this->stripOriginalStyleTags = (bool) $on;
         $this->cssToInlineStyles->setStripOriginalStyleTags($this->stripOriginalStyleTags);
     }
-    
+
     /**
      * Set the encoding to use with the DOMDocument. Default encoding value is "UTF-8".
      *
@@ -146,44 +142,42 @@ class ToInlineStyleEmailConverter {
         $this->encoding = (string) $encoding;
         $this->cssToInlineStyles->setEncoding($this->encoding);
     }
-    
+
     /**
-     * Set the string which represents the CSS for the HTML file to be sent as email. 
-     * 
+     * Set the string which represents the CSS for the HTML file to be sent as email.
+     *
      * @param string $css
      */
     public function setCSS($css){
         $this->css = $css;
         $this->cssToInlineStyles->setCSS($this->css);
     }
-    
+
     /**
-     * Set the string which represents the HTML file to be sent as email. 
-     * 
+     * Set the string which represents the HTML file to be sent as email.
+     *
      * @param string $html
      */
     public function setHTML($html){
         $this->html = $html;
         $this->cssToInlineStyles->setHTML($this->html);
     }
-    
+
     /**
-     * Set the string which represents the HTML file to be sent as email by rendering a template. 
-     * 
+     * Set the string which represents the HTML file to be sent as email by rendering a template.
+     *
      * @param string $view is the view to be rendered, e.g. Acme:MyBundle:MyController:index.html.twig
-     * @param array[optional] $options the array of options to be used for template rendering. This field is optional
+     * @param array[optional] $parameters the array of options to be used for template rendering. This field is optional
      * @throws MissingTemplatingEngineException The TwigEngine must be passed to the constructor, otherwise an exception is thrown
      */
-    public function setHTMLByView($view, array $options = null){
+    public function setHTMLByView($view, array $parameters = array()){
         if(!is_object($this->templating_engine)) throw new MissingTemplatingEngineException("To use this function, a TwigEngine object must be passed to the constructor (use @templating to the the TwigEngine.");
-        if(is_array($options)) setHTML($this->renderView($view, $options));        
-        else setHTML($this->renderView($view));        
+        setHTML($this->templating_engine->render($view, $parameters));
     }
-
 
     /**
      * Generate the HTML ready to be sent as email.
-     * 
+     *
      * @param  bool[optional] $outputXHTML Should we output valid XHTML?
      * @return string return the HTML ready to be sent with an inline-style
      * @throws MissingParamExceptionException the HTML and CSS are mandatory.
@@ -192,8 +186,8 @@ class ToInlineStyleEmailConverter {
         if(is_null($this->html))throw new MissingParamExceptionException("The HTML must be set");
         if(!is_string($this->html))throw new MissingParamExceptionException("The HTML must be a valid string");
         if(!is_string($this->css))throw new MissingParamExceptionException("The CSS must be set");
-        if(!is_string($this->css))throw new MissingParamExceptionException("The CSS must be a valid string");        
-        return  $this->cssToInlineStyles->convert($outputXHTML);       
+        if(!is_string($this->css))throw new MissingParamExceptionException("The CSS must be a valid string");
+        return  $this->cssToInlineStyles->convert($outputXHTML);
     }
 }
 
