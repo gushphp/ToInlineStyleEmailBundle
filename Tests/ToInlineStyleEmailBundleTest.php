2b37853c4327d229139b3c403391643004e0f6a7
robertotru <roberto.trunfio@gmail.com>
2013-01-16 12:10:40 +0100

    Setup for auto-install

---
+++ Tests/ToInlineStyleEmailBundleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RobertoTru\ToInlineStyleEmailBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ToInlineStyleEmailBundleTest extends WebTestCase {
+    public function testIndex() {
+
+        //$client = static::createClient();
+
+        //$crawler = $client->request('GET', '/hello/Fabien');
+
+        //$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    }
+}
